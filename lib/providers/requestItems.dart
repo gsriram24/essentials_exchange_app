b322ceca8362247b72788799c5c60b988ee8765a
Sriram <sgcuber24@gmail.com>
2020-04-23 20:38:01 +0530

    Added request item provider

---
+++ lib/providers/requestItems.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/foundation.dart';
+
+class RequestItem {
+  final String id;
+  final String itemName;
+  final String itemDescription;
+  final String userId;
+  final String imgUrl;
+  final DateTime date;
+
+  RequestItem({
+    @required this.id,
+    @required this.itemName,
+    @required this.itemDescription,
+    @required this.userId,
+    @required this.imgUrl,
+    @required this.date,
+  });
+}
+
+class Requests with ChangeNotifier {
+  List<RequestItem> _requests = [
+    RequestItem(
+      id: '1',
+      itemName: 'Milk',
+      itemDescription:
+          'Lorem Ipsum this is description. Add any item description or specifics here.',
+      userId: 'sgcuber24',
+      imgUrl:
+          'https://images.pexels.com/photos/236010/pexels-photo-236010.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      date: DateTime.now(),
+    ),
+    RequestItem(
+      id: '2',
+      itemName: 'Rubik\'s Cube',
+      itemDescription:
+          'Lorem Ipsum this is description. Add any item description or specifics here. Pls gimme cube my life is bad',
+      userId: 'ashwin2607',
+      imgUrl:
+          'https://images.pexels.com/photos/19677/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      date: DateTime.now(),
+    ),
+    RequestItem(
+      id: '3',
+      itemName: 'Phone Charger',
+      itemDescription:
+          'Lorem Ipsum this is description. Add any item description or specifics here. My phone battery is low I need to charge pls',
+      userId: 'dollarakshay',
+      imgUrl:
+          'https://images.pexels.com/photos/1028674/pexels-photo-1028674.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      date: DateTime.now(),
+    ),
+    RequestItem(
+      id: '4',
+      itemName: 'Soap and Shampoo',
+      itemDescription:
+          'Lorem Ipsum this is description. Add any item description or specifics here. I need to take bath pls give me.',
+      userId: 'yaegerknight',
+      imgUrl:
+          'https://images.pexels.com/photos/545014/pexels-photo-545014.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      date: DateTime.now(),
+    ),
+  ];
+  List<RequestItem> get orders {
+    return [..._requests];
+  }
+}
