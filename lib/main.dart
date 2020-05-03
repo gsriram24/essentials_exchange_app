363db0ef002b0793398123933686d1d3b67cdd46
Sriram <sgcuber24@gmail.com>
2020-05-03 20:09:55 +0530

    Changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Essentially',
         theme: ThemeData.light(),
         routes: {
           '/': (_) => Wrapper(),
